327e595f240cef586c668ba32d292ccc6b500e9f
zdq <1127952115@qq.com>
2019-12-08 18:08:33 +0800

    update

--- src/java/easy/_1_TwoSum.java
+++ src/java/easy/_1_TwoSum.java
@@ -12,8 +12,6 @@ import java.util.Map;
  * 给定 nums = [2, 7, 11, 15], target = 9
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
  * 所以返回 [0, 1]
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/two-sum
  * @author ZDQ
  *
  */
