13ac89785bbdeaee1f2416e90bf9ba55bd300335
ZDQ <1127952115@qq.com>
2021-08-14 16:54:54 +0800

    FirstMissingPositive

---
+++ src/com/zdq/hard/FirstMissingPositive41.java
@@ -0,0 +1,83 @@
+package com.zdq.hard;
+
+/**
+ * 41.
+ * 给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
+ *
+ * 请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案。
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,0]
+ * 输出：3
+ * 示例 2：
+ *
+ * 输入：nums = [3,4,-1,1]
+ * 输出：2
+ * 示例 3：
+ *
+ * 输入：nums = [7,8,9,11,12]
+ * 输出：1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/first-missing-positive
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class FirstMissingPositive41 {
+    public static void main(String[] args) {
+        int[] nums = {3, 4, -1, 1};
+        System.out.println(firstMissingPositive(nums));
+    }
+
+
+    private static int firstMissingPositive(int[] nums) {
+
+        if (nums == null) {
+            return -1;
+        }
+
+        int len = nums.length;
+
+        for (int i = 0; i < len; i++) {
+            // 3,4,-1,1
+            while (nums[i] > 0 && nums[i] <= len && nums[i] != nums[nums[i] - 1]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+
+        for (int i = 0; i < len; i++) {
+            if (nums[i] > len || nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return len + 1;
+    }
+
+    private static int firstMissingPositiveByHash(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int cur = nums[i];
+            //绝对值位运算
+            int num = (cur ^ (cur >> 31)) - (cur >> 31);
+            if (num <= n) {
+                //把小于数组长度的值置为负数
+                int next = nums[num - 1];
+                nums[num - 1] = ~((next ^ (next >> 31)) - (next >> 31)) + 1;;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+}
--- src/com/zdq/hard/MinWindow76.java
+++ src/com/zdq/hard/MinWindow76.java
@@ -1,27 +1,30 @@
 package com.zdq.hard;
 
+import java.util.HashMap;
+
 /**
+ * 最小覆盖子串:
  * 给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
- *
+ * <p>
  * 注意：如果 s 中存在这样的子串，我们保证它是唯一的答案。
- *
+ * <p>
  *  
- *
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 输入：s = "ADOBECODEBANC", t = "ABC"
  * 输出："BANC"
  * 示例 2：
- *
+ * <p>
  * 输入：s = "a", t = "a"
  * 输出："a"
  *  
- *
+ * <p>
  * 提示：
- *
+ * <p>
  * 1 <= s.length, t.length <= 105
  * s 和 t 由英文字母组成
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/minimum-window-substring
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -31,14 +34,102 @@ public class MinWindow76 {
     public static void main(String[] args) {
         String s = "ADOBECODEBANC";
         String t = "ABC";
-
-        System.out.println( minWindow(s, t));
+        System.out.println(minWindow2(s, t));
     }
-
-
 
     private static String minWindow(String s, String t) {
-
-        return "";
+        HashMap<Character, Integer> need = new HashMap<>();
+        HashMap<Character, Integer> window = new HashMap<>();
+        for (char ch : t.toCharArray()) {
+            need.put(ch, need.getOrDefault(ch, 0) + 1);
+        }
+        int left = 0;
+        int right = 0;
+        int valid = 0;
+        int start = 0;
+        int len = Integer.MAX_VALUE;
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                ////这个得改成equals
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+            while (valid == need.size()) {
+                if (right - left < len) {
+                    start = left;
+                    len = right - left;
+                }
+                char d = s.charAt(left);
+                left++;
+                if (need.containsKey(d)) {
+                    //这个得改成equals
+                    if (window.get(d).equals(need.get(d))) {
+                        valid--;
+                    }
+                    window.put(d, window.getOrDefault(d, 0) - 1);
+                }
+            }
+        }
+        return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
     }
+
+
+    private static String minWindow2(String s, String t) {
+        //首先创建的是need数组表示每个字符在t中需要的数量，用ASCII码来保存
+        //加入need[76] = 2，表明ASCII码为76的这个字符在目标字符串t中需要两个，
+        // 如果是负数表明当前字符串在窗口中是多余的，需要过滤掉
+        int[] need = new int[128];
+        //按照字符串t的内容向need中添加元素
+        for (int i = 0; i < t.length(); i++) {
+            need[t.charAt(i)]++;
+        }
+        /*
+        l: 滑动窗口左边界
+        r: 滑动窗口右边界
+        distance: 窗口的长度
+        count: 当次遍历中还需要几个字符才能够满足包含t中所有字符的条件，最大也就是t的长度，避免了遍历need数组查看是否所有元素数量都小于等于0
+        start: 如果有效更新滑动窗口，记录这个窗口的起始位置，方便后续找子串用
+         */
+        int l = 0, r = 0, distance = Integer.MAX_VALUE, count = t.length(), start = 0;
+        //循环条件右边界不超过s的长度
+        while (r < s.length()) {
+            char c = s.charAt(r);
+            //表示t中包含当前遍历到的这个c字符，更新目前所需要的count数大小，应该减少一个
+            if (need[c] > 0) {
+                count--;
+            }
+            //无论这个字符是否包含在t中，need[]数组中对应那个字符的计数都减少1，利用正负区分这个字符是多余的还是有用的
+            need[c]--;
+            //count==0说明当前的窗口已经满足了包含t所需所有字符的条件
+            if (count == 0) {
+                //如果左边界这个字符对应的值在need[]数组中小于0，说明他是一个多余元素，不包含在t内
+                while (l < r && need[s.charAt(l)] < 0) {
+                    //在need[]数组中维护更新这个值，增加1
+                    need[s.charAt(l)]++;
+                    //左边界向右移，过滤掉这个元素
+                    l++;
+                }
+                //如果当前的这个窗口值比之前维护的窗口值更小，需要进行更新
+                if (r - l + 1 < distance) {
+                    //更新窗口值
+                    distance = r - l + 1;
+                    //更新窗口起始位置，方便之后找到这个位置返回结果
+                    start = l;
+                }
+                //先将l位置的字符计数重新加1
+                need[s.charAt(l)]++;
+                //重新维护左边界值和当前所需字符的值count
+                l++;
+                count++;
+            }
+            //右移边界，开始下一次循环
+            r++;
+        }
+        return distance == Integer.MAX_VALUE ? "" : s.substring(start, start + distance);
+    }
+
 }
